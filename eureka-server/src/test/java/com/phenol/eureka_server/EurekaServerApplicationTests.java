8364ee8c5331f3a559f0f747defe145cc421621e
tejpalDarro <tejpaldarro007@gmail.com>
2024-08-01 11:57:59 +0530

    adding eureka server

---
+++ eureka-server/src/test/java/com/phenol/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phenol.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
